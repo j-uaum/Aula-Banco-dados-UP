94c7bac5e61ba8b0e98ebee6e4647cd888005cc5
juaum <jotinhagamer4@gmail.com>
2022-09-01 11:29:49 -0300

    aula 05

---
+++ Aula 05 - script/bkp_quinta_bd_.sql
@@ -0,0 +1,145 @@
+CREATE DATABASE  IF NOT EXISTS `empresa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `empresa`;
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: empresa
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) NOT NULL,
+  `sexo` enum('M','F') DEFAULT NULL,
+  `nascimento` date DEFAULT NULL,
+  `cidade` varchar(20) DEFAULT 'Curitiba',
+  `uf` char(2) DEFAULT 'PR',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Ana','F','1990-07-15','Campo Largo','PR'),(2,'Elaine','F','1995-09-21','Curitiba','PR'),(3,'Fabio','M','1998-09-30','Ponta Grossa','PR'),(4,'Gilberto','M','1996-11-23','Santo André','SP'),(5,'Beatriz','F','1990-07-15','Campo largo','PR');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-01 11:19:25
+CREATE DATABASE  IF NOT EXISTS `nova_empresa` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `nova_empresa`;
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: nova_empresa
+-- ------------------------------------------------------
+-- Server version	8.0.27
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(40) DEFAULT NULL,
+  `idade` int DEFAULT NULL,
+  `sexo` char(1) DEFAULT NULL,
+  `peso` float DEFAULT NULL,
+  `bairro` varchar(20) DEFAULT NULL,
+  `celular` char(10) DEFAULT NULL,
+  `email` varchar(90) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'Marcelo Rossi',52,'M',90,'Novo mundo','99111-1111','marcelolocoloco@outlook.com'),(2,'João Pedro',21,'M',77.5,'Agua verde','99222-2222','jp22@outlook.com'),(3,'Angelica',29,'F',68,'Rebouças','99333-3333','angel@hotmail.com');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `produtos`
+--
+
+DROP TABLE IF EXISTS `produtos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produtos` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) DEFAULT NULL,
+  `estoque` int DEFAULT NULL,
+  `preço` float DEFAULT NULL,
+  `funcionario` varchar(50) DEFAULT NULL,
+  `observação` varchar(200) DEFAULT NULL,
+  `setor` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produtos`
+--
+
+LOCK TABLES `produtos` WRITE;
+/*!40000 ALTER TABLE `produtos` DISABLE KEYS */;
+INSERT INTO `produtos` VALUES (1,'Sabão em pó',100,9,'Alcir Jr.',NULL,'Limpeza'),(2,'Sabão em barra',257,4.5,'Maria Rita Amorim','Algumas barras estão avariadas.','Limpeza'),(3,'Vanish em pó 1 Kg',70,40,'Alcir Jr.',NULL,'Limpeza');
+/*!40000 ALTER TABLE `produtos` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-01 11:19:25
---
+++ Aula 05 - script/script aula 05.sql
@@ -0,0 +1,121 @@
+show databases;
+
+create database empresa;
+use empresa;
+CREATE TABLE clientes (
+				id int NOT NULL AUTO_INCREMENT,
+                nome varchar (40) NOT NULL,
+                sexo enum ('M', 'F'),
+                nascimento date,
+                cidade varchar (20) DEFAULT 'Curitiba' ,
+                uf char (2) DEFAULT 'PR' ,
+                PRIMARY KEY (id) 
+                
+
+) DEFAULT CHARSET = utf8mb4 ;
+
+show tables;
+
+
+/* identifica de forma unica cada registro em uma tabela de um banco de dados */
+
+/*  Na primary key ela precisa ter valor sempre unico e nao pode ser null  e só pode ter 1 chave primaria pode tabela, mas ela pode ser composta por mais de uma coluna */ 
+
+/* cadastro diverso em querys*/
+insert into clientes (id, nome, sexo, nascimento, cidade, uf) values ('1', 'Ana', 'F', '1990-07-15', 'Campo Largo', 'PR');
+
+insert into clientes values
+(default, 'Elaine', 'F', '1995-09-21', default, default),
+(default, 'Fabio', 'M', '1998-09-30', 'Ponta Grossa', default),
+(default, 'Gilberto', 'M', '1996-11-23', 'Santo André', 'SP');
+
+
+/* cadastro personalizado*/
+insert into clientes (nome, sexo, nascimento, cidade, uf) values ('Beatriz', 'F', '1990-07-15', 'Campo largo', 'PR');
+
+
+create database nova_empresa;
+use nova_empresa;
+
+create table clientes(
+			id int not null auto_increment,
+            nome varchar (40),
+            idade int,
+            sexo char(1),
+            peso float,
+            bairro varchar(20),
+            celular char(10),
+
+			primary key (id)
+
+) default charset = utf8mb4;
+
+create table produtos (
+		id int not null auto_increment,
+        nome varchar(100),
+        estoque int,
+        preço float,
+        funcionario varchar(50),
+        observação varchar(200),
+        primary key(id)
+        
+        
+        ) default charset utf8mb4;
+        
+        insert into clientes values 
+        (default, 'Marcelo', 52, 'M', '90.0', 'Novo mundo', '99111-1111'),
+        (default, 'João Pedro', 21, 'M', '77.5', 'Agua verde', '99222-2222'),
+        (default, 'Angelica', 29, 'F', '68.0', 'Rebouças', '99333-3333');
+        
+        
+	insert into produtos values
+    (default, 'Sabão em pó', 100, '9', 'Alcir Jr.', NULL),
+    (default, 'Sabão em barra', 277, '4.50', 'Maria Rita Amorim', 'Algumas barras estão avariadas.'),
+    (default, 'Vanish em pó 1 Kg', 70, '45', 'Alcir Jr.', NULL);
+    
+    alter table produtos add email varchar(90);
+    
+    alter table produtos drop column email;
+    
+    alter table clientes add email varchar(90);
+    
+    update clientes set email = 'marcelo02020@gmail.com' where id = 1;
+    update clientes set email = 'jp22@outlook.com' where id = 2;
+    
+    alter table produtos add setor varchar (20);
+    update produtos set setor = 'Limpeza' where id = 1;
+    update produtos set setor = 'Limpeza' where id = 2;
+    update produtos set setor = 'Limpeza' where id = 3;
+    
+    update clientes set email  = 'angel@hotmail.com' where id = 3;
+    update produtos set estoque = 257 where id = 2;
+    update produtos set preço = '40' where id = 3;
+    
+    desc produtos;
+	select * from clientes;
+    select * from produtos;
+    select nome, email from clientes where sexo= "F";
+    update produtos set observação = null where id = 1;
+    select observação from produtos where id= 1;
+    select * from produtos where observação <> '';
+    
+update clientes set email= 'marcelolocoloco@outlook.com', nome='Marcelo Rossi' where id= 1;
+
+  
+    select nome, email from clientes where bairro='Agua verde';
+    
+ select * from clientes where nome !='Marcelo Rossi' and nome !='Angelica';
+	
+    
+    
+    
+    
+        
+
+
+
+
+
+
+
+
